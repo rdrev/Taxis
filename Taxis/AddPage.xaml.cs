ccc8d7d617c478637c0b674e5d28f3f3b8f1df4d
Никита Шихов <vimimo72@outlook.com>
2020-10-30 11:15:42 +0300

    1

---
+++ Taxis/AddPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Taxis
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPage.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        private Order order = new Order();
+        public AddPage(Order Porder)
+        {
+            InitializeComponent();
+
+            if (Porder != null)
+                order = Porder;
+
+            DataContext = order;
+            TaxiBox.ItemsSource = TaxisEntities1.GetContect().Cars.ToList();
+            DriverBox.ItemsSource = TaxisEntities1.GetContect().Drivers.ToList();
+            ClientBox.ItemsSource = TaxisEntities1.GetContect().Clients.ToList();
+        }
+
+        private void Sava_Click(object sender, RoutedEventArgs e)
+        {
+
+            TaxisEntities1.GetContect().Orders.Add(order);
+            try
+            {
+                TaxisEntities1.GetContect().SaveChanges();
+                Meneger.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Taxis/Tabel.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Taxis
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Meneger.MainFrame.Navigate(new AddPage(null));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                TaxisEntities1.GetContect().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+
+                DGrid.ItemsSource = TaxisEntities1.GetContect().Orders.ToList();
+            }
+        }
+
+        private void View_Click(object sender, RoutedEventArgs e)
+        {
+            Meneger.MainFrame.Navigate(new AddPage((sender as Button).DataContext as Order));
+        }
+    }
+}
